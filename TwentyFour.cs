f845663dd8bcc3dda194ff055500d689f6a08af4
Josiah <stephenjalston@gmail.com>
2018-10-07 11:54:03 +1000

    Proper setup for this repo

---
+++ TwentyFour.cs
@@ -0,0 +1,44 @@
+﻿using Rocket.API.Collections;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+
+namespace CyTic.TwentyFour
+{
+    public class TwentyFour : RocketPlugin<TwentyFourConfiguration>
+    {
+        public override TranslationList DefaultTranslations
+        {
+            get
+            {
+                return new TranslationList()
+                {
+                    {"TwentyFour_max_players_set", "max_players set to {0} from {1}"}
+                };
+            }
+        }
+
+		public int old_max;
+
+		protected override void Load()
+		{
+			if (Configuration.Instance.enabled)
+			{
+				old_max = Provider.maxPlayers;
+				Provider.maxPlayers = (byte)Configuration.Instance.max_players;
+				this.done();
+			}
+		}
+
+		protected override void Unload()
+        {
+            
+        }
+
+		public void done()
+		{
+			string message = Translate("TwentyFour_max_players_set", Configuration.Instance.max_players, old_max);
+			Logger.Log(message);
+		}
+    }
+}
---
+++ TwentyFourConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Rocket.API;
+
+namespace CyTic.TwentyFour
+{
+    public class TwentyFourConfiguration : IRocketPluginConfiguration
+    {
+
+        public int max_players = 24;
+        public bool enabled = true;
+
+        public void LoadDefaults()
+        {
+			int max_players = 24;
+			bool enabled = true;
+        }
+    }
+}
